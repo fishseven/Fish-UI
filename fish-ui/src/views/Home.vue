e277d8d0e5720eeae607e65965d3bc8d2bc320c8
fish <lovefish360@163.com>
2020-09-24 22:20:07 +0800

    home scss

--- fish-ui/src/views/Home.vue
+++ fish-ui/src/views/Home.vue
@@ -6,7 +6,7 @@
         <h2>一个厉害的UI</h2>
         <p class="actions">
             <a href="https://github.com">GithHub</a>
-            <a href="">开始</a>
+            <router-link to="/doc">开始</router-link>
         </p>
     </div>
 </div>
@@ -20,3 +20,29 @@ export default {
     }
 }
 </script>
+
+<style lang="scss" scoped>
+.banner {
+    padding: 100px 0;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    background: lightgreen;
+
+    >.actions {
+        padding: 8px 0;
+
+        a {
+            margin: 0 8px;
+            background: #fff;
+            display: inline-block;
+            $h: 28px;
+            height: $h;
+            line-height: $h;
+            border-radius: $h/2;
+            padding: 0 8px;
+        }
+    }
+}
+</style>
