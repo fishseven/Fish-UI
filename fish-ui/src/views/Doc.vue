577ad26b37c6113cf33aec6cbbc024f85c53a891
fish <lovefish360@163.com>
2020-09-24 22:24:59 +0800

    修改aside css

--- fish-ui/src/views/Doc.vue
+++ fish-ui/src/views/Doc.vue
@@ -32,3 +32,25 @@ export default {
     }
 }
 </script>
+
+<style lang="scss" scoped>
+aside {
+    background: lightblue;
+    width: 150px;
+    padding: 16px;
+    position: fixed;
+    top: 0;
+    left: 0;
+    padding-top: 70px;
+
+    >h2 {
+        margin-bottom: 4px;
+    }
+
+    >ol {
+        >li {
+            padding: 4px 0;
+        }
+    }
+}
+</style>
