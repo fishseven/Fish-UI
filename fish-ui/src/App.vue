7b4975ea1d80cef62dc7673e7ca74a3e96c288d0
fish <lovefish360@163.com>
2020-09-24 23:44:40 +0800

    基本完成官网搭建

--- fish-ui/src/App.vue
+++ fish-ui/src/App.vue
@@ -7,12 +7,20 @@ import {
     ref,
     provide
 } from 'vue';
+import {
+    router
+} from './Router';
 export default {
     name: "App",
     setup() {
         const width = document.documentElement.clientWidth
         const menuVisible = ref(width <= 500 ? false : true)
         provide('vis', menuVisible)
+        router.afterEach(() => {
+            if (width <= 500) {
+                menuVisible.value = false;
+            }
+        })
     }
 };
 </script>
