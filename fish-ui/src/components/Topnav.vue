d30253b8781fb9e5f20402e723344ffb328ace6d
fish <lovefish360@163.com>
2020-10-07 10:09:02 +0800

    基本完成首页

--- fish-ui/src/components/Topnav.vue
+++ fish-ui/src/components/Topnav.vue
@@ -1,11 +1,17 @@
 <template>
 <div class="topnav">
-    <div class="logo">LOGO</div>
-    <ul class="menu">
-        <li>菜单1</li>
-        <li>菜单2</li>
-    </ul>
-    <span class="toggleAside" @click="toggleMenu"></span>
+    <router-link to="/" class="logo">
+        <svg class="icon">
+            <use xlink:href="#icon-king"></use>
+        </svg>
+    </router-link>
+    <li>
+        <router-link to="/doc">文档</router-link>
+    </li>
+
+    <svg v-if="toggleMenuButtonVisible" class="toggleAside" @click="toggleMenu">
+        <use xlink:href="#icon-menu"></use>
+    </svg>
 </div>
 </template>
 
@@ -15,6 +21,12 @@ import {
     Ref
 } from 'vue'
 export default {
+    props: {
+        toggleMenuButtonVisible: {
+            type: Boolean,
+            default: false
+        }
+    },
     setup() {
         const menuVisible = inject < Ref < boolean >> ('vis')
         console.log("topnav 获取的mennuvisible为：" + menuVisible.value)
@@ -30,21 +42,29 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+$color: #007974;
+
 .topnav {
-    background: pink;
+    color: $color;
+    // background: pink;
     display: flex;
     padding: 16px;
     position: fixed;
     top: 0;
     left: 0;
     width: 100%;
-    z-index: 10;
+    z-index: 20;
     justify-content: center;
     align-items: center;
 
     >.logo {
         max-width: 6em;
         margin-right: auto;
+
+        >svg {
+            width: 32px;
+            height: 32px;
+        }
     }
 
     >.menu {
@@ -58,14 +78,15 @@ export default {
     }
 
     >.toggleAside {
-        width: 24px;
-        height: 24px;
-        background: red;
+        width: 32px;
+        height: 32px;
+        // background: red;
         position: absolute;
         left: 16px;
         top: 50%;
         transform: translateY(-50%);
         display: none;
+        background: fade-out(black, 0.9);
     }
 
     @media (max-width:500px) {
--- fish-ui/src/views/Doc.vue
+++ fish-ui/src/views/Doc.vue
@@ -1,7 +1,7 @@
 <template>
 <div>
     <div class="layout">
-        <Topnav class="nav" />
+        <Topnav :toggleMenuButtonVisible="true" class="nav" />
         <div class="content">
             <aside v-if="menuVisible">
                 <h2>组件列表</h2>
